6ab6c845af9d5b279f3d6e791206e6b8a94bfe5d
StijnWillems <stijn.willems@icapps.com>
2017-03-27 15:39:19 +0200

    Add animator ViewController

---
+++ LottieDropper/LottieAnimatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  LottieAnimatorViewController.h
+//  LottieDropper
+//
+//  Created by Stijn Willems on 27/03/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LottieAnimatorViewController : UIViewController
+
+@end
