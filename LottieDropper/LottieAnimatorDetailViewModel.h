0f323adbe21fd7090290623c650489b02b491b0c
StijnWillems <stijn.willems@icapps.com>
2017-03-27 15:40:25 +0200

    Add detail

---
+++ LottieDropper/LottieAnimatorDetailViewModel.h
@@ -0,0 +1,13 @@
+//
+//  LottieAnimatorDetailViewModel.h
+//  LottieDropper
+//
+//  Created by Stijn Willems on 27/03/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LottieAnimatorDetailViewModel : NSObject
+
+@end
---
+++ LottieDropper/LottieAnimatorDetailViewModel.m
@@ -0,0 +1,13 @@
+//
+//  LottieAnimatorDetailViewModel.m
+//  LottieDropper
+//
+//  Created by Stijn Willems on 27/03/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import "LottieAnimatorDetailViewModel.h"
+
+@implementation LottieAnimatorDetailViewModel
+
+@end
