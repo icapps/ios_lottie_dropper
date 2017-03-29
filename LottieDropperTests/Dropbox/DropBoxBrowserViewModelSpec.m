eb347f797fdae9bd762350a932ab5237654f9cc4
Stefan Adams <stefan.adams@icapps.com>
2017-03-29 11:46:05 +0200

    Added testfile for DropBoxBrowserViewModel

---
+++ LottieDropperTests/Dropbox/DropBoxBrowserViewModelSpec.m
@@ -0,0 +1,29 @@
+//
+//  DropBoxBrowserViewModelSpec.m
+//  LottieDropper
+//
+//  Created by Stefan Adams on 29/03/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "DropboxBrowserViewModel.h"
+
+@import Quick;
+@import Nimble;
+
+@interface DropBoxBrowserViewModelSpec : QuickSpec
+
+@property (strong, nonatomic) DropboxBrowserViewModel* viewModel;
+
+@end
+
+@implementation DropBoxBrowserViewModelSpec
+
+- (void)spec {
+    
+}
+
+@end
+
