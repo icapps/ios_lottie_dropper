96934debc0847f8e4d05060558e708914dfe0d88
Ben Algoet <ben.algoet@icapps.com>
2017-04-11 13:45:02 +0200

    #7 add Error viewController

---
+++ LottieDropper/LottieAnimator/ErrorLogViewController.h
@@ -0,0 +1,13 @@
+//
+//  ErrorLogViewController.h
+//  LottieDropper
+//
+//  Created by Ben Algoet on 11/04/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ErrorLogViewController : UIViewController
+
+@end
---
+++ LottieDropper/LottieAnimator/ErrorLogViewController.m
@@ -0,0 +1,23 @@
+//
+//  ErrorLogViewController.m
+//  LottieDropper
+//
+//  Created by Ben Algoet on 11/04/2017.
+//  Copyright © 2017 icapps. All rights reserved.
+//
+
+#import "ErrorLogViewController.h"
+
+@interface ErrorLogViewController ()
+
+@end
+
+@implementation ErrorLogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
