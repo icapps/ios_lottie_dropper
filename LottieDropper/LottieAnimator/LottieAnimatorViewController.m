cd601b8a601d1cc580fbf1097df5177070ec078c
Stefan Adams <stefan.adams@icapps.com>
2017-04-12 17:16:47 +0200

    #2.1 Added hex textfield to the colorpicker view.

--- LottieDropper/LottieAnimator/LottieAnimatorViewController.m
+++ LottieDropper/LottieAnimator/LottieAnimatorViewController.m
@@ -16,9 +16,14 @@
 @interface LottieAnimatorViewController ()
 @property (strong, nonatomic) LOTAnimationView *animation;
 @property (weak, nonatomic) IBOutlet UISlider *slider;
+
+// Color picker
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *colorViewTopConstraint;
 @property (weak, nonatomic) IBOutlet NKOColorPickerView *colorPickerView;
+@property (weak, nonatomic) IBOutlet UIView *colorPickerBackgroundView;
+@property (weak, nonatomic) IBOutlet UITextField *hexTextfield;
 @property (assign) BOOL isColorPickerOpen;
+@property (strong, nonatomic) UIColor *savedColor;
 
 #pragma mark: Toolbar buttons
 @property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
@@ -42,6 +47,7 @@
 	}];
     
     self.colorPickerView.didChangeColorBlock = ^(UIColor *color){
+        self.hexTextfield.text = [self hexStringFromColor:color];
         self.view.backgroundColor = color;
     };
     
@@ -51,7 +57,8 @@
 #pragma mark: Setup UI
 
 - (void) setupUI {
-    self.colorPickerView.layer.cornerRadius = 10;
+    self.colorPickerBackgroundView.layer.cornerRadius = 10;
+    [self.colorPickerView setColor:UIColor.whiteColor];
 }
 
 #pragma mark: Toolbar actions
@@ -95,7 +102,7 @@
 		_animation = [LOTAnimationView animationFromJSON:self.dropboxDetail.json];
 		_animation.translatesAutoresizingMaskIntoConstraints = NO;
 
-		[self.view addSubview:self.animation];
+        [self.view insertSubview:self.animation belowSubview:self.colorPickerBackgroundView];
 
 		[[[self.animation centerXAnchor] constraintEqualToAnchor:self.view.centerXAnchor] setActive:YES];
 		[[[self.animation centerYAnchor] constraintEqualToAnchor:self.view.centerYAnchor] setActive:YES];
@@ -137,12 +144,12 @@
     [self.toolbar setItems:toolbarButtons];
 }
 
-#pragma mark: Open/Close color picker.
+#pragma mark: Color Picker.
 
 - (void)shouldOpenColorPicker: (BOOL)shouldOpen {
     if (shouldOpen) {
         self.isColorPickerOpen = true;
-        self.colorViewTopConstraint.constant = -252;
+        self.colorViewTopConstraint.constant = -302;
         
         [UIView animateWithDuration:0.35 animations:^{
             [self.view layoutIfNeeded];
@@ -155,6 +162,16 @@
             [self.view layoutIfNeeded];
         }];
     }
+}
+
+- (NSString *)hexStringFromColor:(UIColor *)color {
+    const CGFloat *components = CGColorGetComponents(color.CGColor);
+    
+    CGFloat r = components[0];
+    CGFloat g = components[1];
+    CGFloat b = components[2];
+    
+    return [NSString stringWithFormat:@"#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255)];
 }
 
 @end
